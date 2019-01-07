22228243852d75bf59facfc04c058c538ee61930
Godspeed3 <40567234+Godspeed3@users.noreply.github.com>
2019-01-07 13:34:01 +0100

    Update SSH Cisco macro script.py

--- SSH Cisco macro script.py
+++ SSH Cisco macro script.py
@@ -1,56 +1,56 @@
-from getpass import Getpass
+from getpass import getpass
 from netmiko import Netmiko
 
 
 while True:
     try:
-        net_connect = Netmiko(host = input('Enter host ip: '),
-            username = 'admin',
-            password = getpass(),
-            device_type = 'cisco_ios'
-            )
+        net_connect = Netmiko(host=input('Enter host ip: '),
+                              username='admin',
+                              password=getpass(),
+                              device_type='cisco_ios'
+                              )
         break
     except Exception:
         print('Incorrect host or password, please try again.')
         continue
 
-        
+
 def ssh_script():
     input_interfaces = input('enter interfaces, "1/1" or "1/1-5" seperated by a space: ').split()
-    
+
     while True:
         try:
-          macro = input('enter voip or ap macro: ')
-          if macro == 'ap' or macro == 'access point':
-              ap_vlan = int(input('enter access point vlan: '))
-              macro_command = f'macro apply ap $vlan {ap_vlan}'
-              desciption_input = input('enter port description: ')
-              description_command = f'description {desciption_input}'
+            macro = input('enter voip or ap macro: ')
+            if macro == 'ap' or macro == 'access point':
+                ap_vlan = int(input('enter access point vlan: '))
+                macro_command = f'macro apply ap $vlan {ap_vlan}'
+                description_input = input('enter port description: ')
+                description_command = f'description {description_input}'
 
-              for i in input_interfaces:
-                  interface_command = 'interface range gi' + i
-                  commands = [interface_command, macro_command, description_command]
-                  net_connect.send_config_set(commands)
-              break
-          elif macro == 'voip' or macro == 'voice':
-              access_vlan = int(input('enter access vlan: '))
-              voice_vlan = int(input('enter voice vlan: '))
-              macro_command = f'macro apply voip $vlan {access_vlan} $vvlan {voice_vlan}'
-              desciption_input = input('enter port description: ')
-              description_command = f'description {desciption_input}'
+                for i in input_interfaces:
+                    interface_command = 'interface range gi' + i
+                    commands = [interface_command, macro_command, description_command]
+                    net_connect.send_config_set(commands)
+                break
+            elif macro == 'voip' or macro == 'voice':
+                voice_vlan = int(input('enter voice vlan: '))
+                access_vlan = int(input('enter access vlan: '))
+                macro_command = f'macro apply voip $vlan {access_vlan} $vvlan {voice_vlan}'
+                description_input = input('enter port description: ')
+                description_command = f'description {description_input}'
 
-              for i in input_interfaces:
-                  interface_command = 'interface range gi' + i
-                  commands = [interface_command, macro_command, description_command]
-                  net_connect.send_config_set(commands)
-              break
-          else:
-              print('please make a valid selection.')
-              continue
+                for i in input_interfaces:
+                    interface_command = 'interface range gi' + i
+                    commands = [interface_command, macro_command, description_command]
+                    net_connect.send_config_set(commands)
+                break
+            else:
+                print('please make a valid selection.')
+                continue
         except Exception:
             print('please make a valid selection.')
 
-            
+
 ssh_script()
 
 
@@ -72,4 +72,3 @@ while True:
 
 print('Disconnecting...')
 net_connect.disconnect()
-
