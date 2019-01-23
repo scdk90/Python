2ececd0d11c4b38e8f777ef6fb10257cb72e30f6
Godspeed3 <40567234+Godspeed3@users.noreply.github.com>
2019-01-23 12:37:05 +0100

    Update Telnet Cisco Unused ports.py

--- Telnet Cisco Unused ports.py
+++ Telnet Cisco Unused ports.py
@@ -3,20 +3,18 @@ import telnetlib
 
 
 host = input('Enter host ip: ')
-password = getpass.getpass()
+password = getpass.getpass().encode(encoding='ascii')
 
-tn = telnetlib.Telnet(host)
+tn = telnetlib.Telnet(host, timeout = 5)
 
-tn.read_until(b"Password: ")
-tn.write(password.encode('ascii') + b"\r\n")
-tn.write(b"enable\r\n")
-tn.write(password.encode('ascii') + b"\r\n")
+tn.read_until(b'Password: ')
+tn.write(password + b'\r\n')
+tn.write(b'enable' + b'\r\n')
+tn.write(password + b'\r\n')
 
-tn.write(b"show int | i proto.*notconnect|proto.*administratively down|Last in.* [6-9]w|Last in.*[0-9][0-9]w|[0-9]y|disabled|Last input never, output never, output hang never" + b"\r\n")
+tn.write(b'show int | i proto.*notconnect|proto.*administratively down|Last in.* [6-9]w|Last in.*[0-9][0-9]w|[0-9]y|disabled|Last input never, output never, output hang never' + b'\r\n')
+tn.write(b' ' + b' ' + b' ')
 
-tn.write(b"exit\r\n")
-
-print(tn.read_all().decode('ascii'))
+print(tn.read_until(b'exit', timeout = 3).decode('ascii'))
 
 input('press enter to exit: ')
-
